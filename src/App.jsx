c4775e67267a7bb214cdf978cbeda23d39f777f9
Luis Maiworm <luis.maiworm@outlook.de>
2023-11-02 19:17:11 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <a className="bg-slate-400" href="">Test</a>
+    </>
+  )
+}
+
+export default App
